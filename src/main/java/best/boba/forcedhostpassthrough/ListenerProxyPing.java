9b310ef8a3d00625e4fe35efc440b2535c4e4ace
BBaoVanC <bbaovanc@bbaovanc.com>
2022-01-25 01:02:04 -0600

    It works

--- src/main/java/best/boba/forcedhostpassthrough/ListenerProxyPing.java
+++ src/main/java/best/boba/forcedhostpassthrough/ListenerProxyPing.java
@@ -8,6 +8,7 @@ import com.velocitypowered.api.proxy.InboundConnection;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import com.velocitypowered.api.proxy.server.ServerPing;
 import com.velocitypowered.api.util.Favicon;
+import com.velocitypowered.api.util.ModInfo;
 import net.kyori.adventure.text.Component;
 import org.slf4j.Logger;
 
@@ -15,6 +16,7 @@ import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 public class ListenerProxyPing {
     final Config config;
@@ -24,12 +26,9 @@ public class ListenerProxyPing {
 
     @Subscribe(order = PostOrder.FIRST)
     public void onProxyPing(ProxyPingEvent event) {
-        Logger logger = this.config.logger();
-
         InboundConnection connection = event.getConnection();
         Optional<InetSocketAddress> optionalVirtualHost = connection.getVirtualHost();
         if (optionalVirtualHost.isEmpty()) {
-            logger.info("Virtual host was empty");
             return;
         }
 
@@ -39,12 +38,8 @@ public class ListenerProxyPing {
         Map<String, List<String>> forcedHosts = this.config.server().getConfiguration().getForcedHosts();
         List<String> tryBackends = forcedHosts.get(hostString);
         if (tryBackends == null) {
-            logger.info("No forced hosts found for: " + hostString);
             return;
         }
-
-        logger.info("Trying the following hosts for ping passthrough: " +
-                String.join(", ", tryBackends));
 
 
         ServerPing.Builder serverPing = event.getPing().asBuilder();
@@ -58,21 +53,45 @@ public class ListenerProxyPing {
 
             ServerPing backendPing;
             try {
-                backendPing = backend.ping().join();
-            } catch (Exception e) {
-                e.printStackTrace();
+                backendPing = backend.ping().get();
+            }
+            catch (InterruptedException | ExecutionException e) {
                 continue;
             }
 
-            if (backendPing == null) {
-                continue;
-            }
             serverPing.description(backendPing.getDescriptionComponent());
+            serverPing.version(backendPing.getVersion());
+
+            Optional<Favicon> optionalFavicon = backendPing.getFavicon();
+            if (optionalFavicon.isPresent()) {
+                serverPing.favicon(optionalFavicon.get());
+            } else {
+                serverPing.clearFavicon();
+            }
+
+            Optional<ModInfo> optionalModInfo = backendPing.getModinfo();
+            if (optionalModInfo.isPresent()) {
+                serverPing.mods(optionalModInfo.get());
+            } else {
+                serverPing.clearMods();
+            }
+
+            Optional<ServerPing.Players> optionalPlayers = backendPing.getPlayers();
+            if (optionalPlayers.isPresent()) {
+                ServerPing.Players players = optionalPlayers.get();
+                serverPing.onlinePlayers(players.getOnline());
+                serverPing.maximumPlayers(players.getMax());
+                serverPing.samplePlayers(players.getSample().toArray(new ServerPing.SamplePlayer[0]));
+                // ^^ this last line needs testing
+            } else {
+                serverPing.nullPlayers();
+            }
 
             event.setPing(serverPing.build());
-            break;
+            break; // do not try and fall back to any further backends because we're done
         }
 
         // if none of the backends responded, then serverPing should have never been updated.
+        // that means that it will fall back to the default in Velocity config
     }
 }
